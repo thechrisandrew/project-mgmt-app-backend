1b8147b6d6858968cd56bbdb21458384156a9939
thechrisandrew <christopherandrew612@gmail.com>
2022-09-25 16:19:12 +0700

    feat: Add initial boilerplate code

---
+++ index.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const express = require("express");
+const {
+	ApolloServerPluginLandingPageGraphQLPlayground,
+	ApolloServerPluginLandingPageDisabled,
+	ApolloServerPluginLandingPageProductionDefault,
+} = require("apollo-server-core");
+const { ApolloServer } = require("apollo-server-express");
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+
+const connectDB = require("./config/db");
+
+connectDB(); //Make connection to database
+
+async function startServer() {
+	const app = express();
+
+	const apolloServer = new ApolloServer({
+		typeDefs,
+		resolvers,
+		settings: {
+			"editor.theme": "light",
+		},
+		csrfPrevention: true,
+		cache: "bounded",
+		introspection: process.env.NODE_ENV === "development",
+		playground: process.env.NODE_ENV === "development",
+		plugins: [
+			process.env.NODE_ENV === "development"
+				? ApolloServerPluginLandingPageGraphQLPlayground()
+				: ApolloServerPluginLandingPageProductionDefault({
+						footer: false,
+				  }),
+		],
+	});
+
+	await apolloServer.start();
+
+	apolloServer.applyMiddleware({ app });
+
+	app.use((req, res) => {
+		res.send("Hello from express apollo server!");
+	});
+
+	const port = process.env.PORT || 4000;
+	app.listen(port, () => {
+		console.log(`Server is running in ${process.env.NODE_ENV} mode at http://localhost:${port}`);
+		console.log(`GraphQL path: http://localhost:${port}${apolloServer.graphqlPath}`);
+	});
+}
+startServer();
